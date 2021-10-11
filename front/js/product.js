099c9a91fa36fdda0cba5c5d209db86fa3440527
Damien Carré <damiencarre07@gmail.com>
2021-10-11 18:55:43 +0200

    fetch id and display post

--- front/js/product.js
+++ front/js/product.js
@@ -1,40 +1,89 @@
+let searchParams =  new URLSearchParams(location.search);
+let paramsId =  searchParams.get("id");
+let urlId = `http://localhost:3000/api/products/${paramsId}`;
+let product = "";
 
+//connect API and fetch post
+function getPostId(){
+  fetch(urlId)
+  .then(function(res){
+    if (res) {
+      return res.json();
+    }
+  })
+  .then(async function(value) {
+    product = await value;
+    if(product){
+      getPost(product);
+      
+      function getPost(product){
+        let item = document.querySelector('.item');
+        let article = document.createElement('article');
+        item.appendChild(article);
+        article.innerHTML =  `
+          <div class="item__img">
+          <img src="${product.imageUrl}" alt="${product.altTxt}">
+          </div>
+          <div class="item__content">
+  
+            <div class="item__content__titlePrice">
+              <h1 id="title">${product.name}</h1>
+              <p>Prix : <span id="price">${product.price}</span>€</p>
+            </div>
+  
+            <div class="item__content__description">
+              <p class="item__content__description__title">Description :</p>
+              <p id="description">${product.description}</p>
+            </div>
+  
+            <div class="item__content__settings">
+              <div class="item__content__settings__color">
+                <label for="color-select">Choisir une couleur :</label>
+                <select name="color-select" id="colors">
+                    <option value="">--SVP, choisissez une couleur --</option>
+                    <option value="vert">vert</option>
+                    <option value="blanc">blanc</option>
+                </select>
+              </div>
+  
+              <div class="item__content__settings__quantity">
+                <label for="itemQuantity">Nombre d'article(s) (1-100) :</label>
+                <input type="number" name="itemQuantity" min="1" max="100" value="0" id="quantity">
+              </div>
+            </div>
+  
+            <div class="item__content__addButton">
+              <button id="addToCart">Ajouter au panier</button>
+            </div>
+  
+          </div>`;
 
- //   <article>
-  //   <div class="item__img">
-  //     <img src="../images/logo.png" alt="Photographie d'un canapé">
-  //   </div>
-  //   <div class="item__content">
+       
+        //   function selectColor(){
+        //     let selectColors = document.getElementById(colors);
+        //     let options = document.querySelectorAll('#colors option');
+        //     let option = options.value;
 
-  //     <div class="item__content__titlePrice">
-  //       <h1 id="title">Nom du produit</h1>
-  //       <p>Prix : <span id="price">42</span>€</p>
-  //     </div>
+        //     selectColors.appendChild(option);
+        //     console.log(selectColors);
+        //   console.log(options);
+        //     if (options > colors[0]){
+        //       for(let i = 1; i = options.length; i++){
+        //         `<option value="">--SVP, ${colors[i]} --</option>`
+              
+        //       }
 
-  //     <div class="item__content__description">
-  //       <p class="item__content__description__title">Description :</p>
-  //       <p id="description">Dis enim malesuada risus sapien gravida nulla nisl arcu.</p>
-  //     </div>
-
-  //     <div class="item__content__settings">
-  //       <div class="item__content__settings__color">
-  //         <label for="color-select">Choisir une taille :</label>
-  //         <select name="color-select" id="colors">
-  //             <option value="">--SVP, choisissez une couleur --</option>
-  //             <option value="vert">vert</option>
-  //             <option value="blanc">blanc</option>
-  //         </select>
-  //       </div>
-
-  //       <div class="item__content__settings__quantity">
-  //         <label for="itemQuantity">Nombre d'article(s) (1-100) :</label>
-  //         <input type="number" name="itemQuantity" min="1" max="100" value="0" id="quantity">
-  //       </div>
-  //     </div>
-
-  //     <div class="item__content__addButton">
-  //       <button id="addToCart">Ajouter au panier</button>
-  //     </div>
-
-  //   </div>
-  // </article>
+            
+        //     }
+        // }
+        // selectColor();
+      }
+    }
+  })
+  .catch(function(err) {
+    //displayed error
+    console.log('erreur');
+    
+  })
+};
+getPostId()
--- front/js/script.js
+++ front/js/script.js
@@ -1,9 +1,8 @@
 //APPROCHE MODULES
-
 var app = {
   url : `http://localhost:3000/api/products`,
   products : "",
-
+  
   init: function(){
     app.getPosts();
   },
@@ -23,6 +22,7 @@ var app = {
     .catch(function(err) {
       //displayed error
       app.displayError();
+      
     })
   },
   
@@ -47,15 +47,16 @@ var app = {
         articleTitle.innerHTML = product.name;
         articleP.innerHTML = product.description;
       }
-       const searchParams = new URLSearchParams(location.search);
-       searchParams.get("id");
-       if(searchParams.has('id')){
-       window.location.pathname = `/front/html/product.html`;
+      
+      const searchParams = new URLSearchParams(location.search);
+      searchParams.get("id");
+      if(searchParams.has('id')){
+        location.pathname = `/front/html/product.html`;
       }
       else{
-        location.pathname; 
+        // location.pathname;
       }
-
+      
       let itemsArticle = document.querySelectorAll('#items a');
       for(let i  = 0; i<itemsArticle.length; i++){
         let itemArticle = itemsArticle[i];
@@ -69,77 +70,67 @@ var app = {
     }
     
   } ,
-  getDisplayItem: function(e){
-    console.log(e);
-  },
-  
-  //display item
+ 
   
   // displayed error
-  displayError: function(){
-    let titleH2 = document.querySelector('.limitedWidthBlock .titles h2');
-    titleH2.innerHTML = "Nous sommes désolés, une erreur est survenue" ;
-  }
+    displayError: function(){
+      let titleH2 = document.querySelector('.limitedWidthBlock .titles h2');
+      titleH2.innerHTML = "Nous sommes désolés, une erreur est survenue" ;
+    }
   
-  
+
+
 };
 document.addEventListener('DOMContentLoaded', app.init);
 
 
-// export { getPosts };
 
-// console.log( getPosts );
+
+
 // js version 1
 // connect API and promise
 // const url = `http://localhost:3000/api/products`;
 // fetch(url)
 // .then(function(res) {
-//   if (res) {
-//     return res.json();
-//   }
-// })
-// .then(function(value) {
-//   //return of promise
-//   let products = value;
-//   if (products){
-//     for(let product of products){
-//           // const productId = window.location.href;
-//           // console.log(productId);
+  //   if (res) {
+    //     return res.json();
+    //   }
+    // })
+    // .then(function(value) {
+      //   //return of promise
+      //   let products = value;
+      //   if (products){
+        //     for(let product of products){
+          //           // const productId = window.location.href;
+          //           // console.log(productId);
           
-//           let items = document.querySelector('#items');
-//           let a = document.createElement('a');
-//           items.appendChild(a);
-//           let article = document.createElement('article');
-//           a.appendChild(article);
-//           let articleImg = document.createElement('img');
-//           articleImg.src= product.imageUrl;
-//           article.appendChild(articleImg);
-//           let articleTitle = document.createElement('h3');
-//           article.appendChild(articleTitle);
-//           let articleP = document.createElement('p');
-//           article.appendChild(articleP);
-//           // a.href += productId;
-//           console.log(a);
-//           articleImg.setAttribute('alt', product.altTxt);
-//           articleTitle.innerHTML = product.name;
-//           articleP.innerHTML = product.description;
-//         }
-//       }
-// })
-// .catch(function(err) {
-//   //displayed error
-//   let titleH2 = document.querySelector('.limitedWidthBlock .titles h2');
-//   titleH2.innerHTML = "Nous sommes désolés, une erreur est survenue" ;
-//   err = titleH2; 
-// })
-
-
-
-
-
-
-
-
-
-
-
+          //           let items = document.querySelector('#items');
+          //           let a = document.createElement('a');
+          //           items.appendChild(a);
+          //           let article = document.createElement('article');
+          //           a.appendChild(article);
+          //           let articleImg = document.createElement('img');
+          //           articleImg.src= product.imageUrl;
+          //           article.appendChild(articleImg);
+          //           let articleTitle = document.createElement('h3');
+          //           article.appendChild(articleTitle);
+          //           let articleP = document.createElement('p');
+          //           article.appendChild(articleP);
+          //           // a.href += productId;
+          //           console.log(a);
+          //           articleImg.setAttribute('alt', product.altTxt);
+          //           articleTitle.innerHTML = product.name;
+          //           articleP.innerHTML = product.description;
+          //         }
+          //       }
+          // })
+          // .catch(function(err) {
+            //   //displayed error
+            //   let titleH2 = document.querySelector('.limitedWidthBlock .titles h2');
+            //   titleH2.innerHTML = "Nous sommes désolés, une erreur est survenue" ;
+            //   err = titleH2; 
+            // })
+            
+            
+            
+        
