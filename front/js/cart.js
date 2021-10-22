c22244ac294039ba796e4407fee39307c37c3c4e
Damien Carré <damiencarre07@gmail.com>
2021-10-22 14:07:07 +0200

    quantity of cart

--- front/js/cart.js
+++ front/js/cart.js
@@ -2,7 +2,15 @@
 let kanaStorage = JSON.parse(localStorage.getItem("kanap")); //récupération du localstorage
 
 let price ="document.querySelector('.cart__item__content__titlePrice p')";
+ //gestion du tarif selon les quantité
 
+ function UpdateQuantity(e){
+     console.log(e.target.value);
+  // let itemQuantitys = document.querySelectorAll('.itemQuantity');
+  // for(let itemQuantity of itemQuantitys){
+  // }
+
+}
 function getCart(){  //fonction qui affiche le panier
   for(kanap of kanaStorage){ 
     const cartItems = document.getElementById('cart__items');
@@ -14,53 +22,69 @@ function getCart(){  //fonction qui affiche le panier
     cartArticle.setAttribute('data-id', kanap.id);
     cartArticle.innerHTML =  `
     <div class="cart__item__img">
-    <img src="${kanap.image}" alt="${kanap.altTxt}">
+      <img src="${kanap.image}" alt="${kanap.altTxt}">
     </div>
     <div class="cart__item__content">
-    <div class="cart__item__content__titlePrice">
-    <h2>${kanap.name}</h2>
-    <p>${kanap.price} €</p>
-    </div>
+      <div class="cart__item__content__titlePrice">
+        <h2>${kanap.name}</h2>
+        <p>${kanap.price} €</p>
+      </div>
     <div class="cart__item__content__settings">
     <div class="cart__item__content__settings__quantity">
-    <p>Qté : </p>
-    <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${kanap.quantity}">
+      <p>Qté : </p>
+      <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${kanap.quantity}" onChange="updateQuantity(e)">
     </div>
-    <div class="cart__item__content__settings__delete">
-    <p class="deleteItem"onClick= "deleteProduct(${kanap.id})">Supprimer</p>
-    </div>
+      <div class="cart__item__content__settings__delete">
+        <p class="deleteItem" onClick= "deleteProduct(${kanap.id})">Supprimer</p>
+      </div>
     </div>`
     
   }
   
- 
-  //gestion du tarif selon les quantité
-  // function handleUpdateQuantity(){
-
-    let kanapQuantitys = document.querySelectorAll('.itemQuantity');
-    for (i=0; i < kanapQuantitys.length; i++){
-      kanapQuantitys[i].addEventListener('change', (e)=>{
-        quantity = e.target.value;
-        kanap.price = (quantity * kanap.price);
-        
-        // return kanap.price;
-        // let price = document.querySelector('.cart__item__content__titlePrice p');
-        // price.innerHTML =  `${quantity * parseFloat(kanap.price)} €`;
-      })
   
-    }
-  // }
-}
-getCart();
-
-
-// let kanapQuantity = document.querySelectorAll('.itemQuantity');
-//   for (quantity of kanapQuantity){
-//     // console.log(quantity.value);
-//     quantity.addEventListener('change', ()=>{
-//     let price = document.querySelector('.cart__item__content__titlePrice p');
-//     let kanapPrice = (parseInt(quantity) * parseFloat(kanap.price) );
-//     price.innerHTML =  `${quantity * kanapPrice } €`;
+  
+  
+  // function UpdateQuantity(e){
+    //   for(let itemQuantity of itemQuantitys){
+      //     console.log(e.target.value);
+      //   }
+      
+      // }
+      // UpdateQuantity();
+      // function handleUpdateQuantity(){
+        
+        //     let kanapQuantitys = document.querySelector('.itemQuantity');
+        //     let price = document.querySelector('.cart__item__content__titlePrice p');
+        //     for (i=0; i<kanapQuantitys.length; i++){
+          //       kanapQuantitys[i].addEventListenerAll('change', (e)=>{
+            //         quantity = e.target.value;
+            //         kanapPrice = (parseInt(quantity) * parseFloat(kanap.price));
+            
+            // console.log(quantity, kanap.price, kanapPrice);
+            //         price.innerHTML =  `${kanapPrice} €`;
+            //       })
+  
+            //     }
+            // }
+          }
+          getCart();
+          
+          //gestion du tarif selon les quantité
+          function updateQuantity(e){
+        
+            // let itemQuantitys = document.querySelectorAll('.itemQuantity');
+            console.log(e);
+          
+          
+          }
+          
+          // let kanapQuantity = document.querySelectorAll('.itemQuantity');
+          //   for (quantity of kanapQuantity){
+            //     // console.log(quantity.value);
+            //     quantity.addEventListener('change', ()=>{
+              //     let price = document.querySelector('.cart__item__content__titlePrice p');
+              //     let kanapPrice = (parseInt(quantity) * parseFloat(kanap.price) );
+              //     price.innerHTML =  `${quantity * kanapPrice } €`;
 //     });
 //   };
 //   ;
